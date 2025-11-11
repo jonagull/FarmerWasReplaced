8c4e0c08b93d7e9b8f9a5afa09e5d7e4c81c8a59
boien <jonathangullik@gmail.com>
2025-11-11 10:50:49 +0100

    init

---
+++ h.py
@@ -0,0 +1,60 @@
+#move(South)
+
+#harvest()
+#till()
+#plant(Entities.Carrot)
+#move(North)
+
+def moveToNext():
+	move(East)
+	move(North)
+	
+def plantPumpkin():
+	harvest()
+	if (get_ground_type() == Grounds.Grassland):
+		till()
+	plant(Entities.Pumpkin)
+
+	
+def plantCarrot():
+	harvest()
+	if (get_ground_type() == Grounds.Grassland):
+		till()
+	plant(Entities.Carrot)
+	
+def plantBush():
+	harvest()
+	if (get_ground_type() == Grounds.Grassland):
+		till()
+	plant(Entities.Bush)
+	
+	
+def plantTree():
+	if can_harvest():
+		harvest()
+	#if (get_ground_type() == Grounds.Grassland):
+		#till()
+	plant(Entities.Tree)
+	use_item(Items.Water)
+	
+def plantSunflower():
+	if can_harvest():
+		harvest()
+	if (get_ground_type() == Grounds.Grassland):
+		till()
+	plant(Entities.Sunflower)
+	
+	
+def plantGrass():
+	if can_harvest():
+		harvest()
+	if (get_ground_type() == Grounds.Soil):
+		till()
+		
+		
+def plantCactus():
+	if can_harvest():
+		harvest()
+	if (get_ground_type() == Grounds.Grassland):
+		till()
+	plant(Entities.Cactus)
---
+++ main.py
@@ -0,0 +1,70 @@
+import h
+top_pos = 15
+
+change_hat(Hats.Pumpkin_Hat)
+
+while True:
+	
+	#if get_pos_x() == 1 :
+		#h.plantTree()
+		#move(North)
+		
+		#if get_pos_y() == top_pos:
+			#h.plantTree()
+			#h.moveToNext()
+			#
+	if get_pos_x() == 0:
+		h.plantGrass()
+		move(North)
+
+		if get_pos_y() == top_pos:
+			h.plantGrass()
+			h.moveToNext()
+			
+			
+			
+	elif get_pos_x() == 2 or get_pos_x() == 3 or get_pos_x() == 1:
+		h.plantCarrot()
+		move(North)
+		
+		if get_pos_y() == top_pos:
+			h.plantCarrot()
+			h.moveToNext()
+			
+	elif  get_pos_x() == 4:
+		h.plantCactus()
+		move(North)
+		
+		if get_pos_y() == top_pos:
+			h.plantCactus()
+			h.moveToNext()
+		
+			
+	elif get_pos_x() == 5:
+		h.plantSunflower()
+		move(North)
+
+		if get_pos_y() == top_pos:
+			h.plantSunflower()
+			h.moveToNext()
+			
+	elif get_pos_x() == 6 or get_pos_x() == 1:
+		h.plantTree()
+		move(North)
+
+		if get_pos_y() == top_pos:
+			h.plantTree()
+			h.moveToNext()
+			
+	else:
+		h.plantPumpkin()
+		move(North)
+
+		if get_pos_y() == top_pos:
+			h.plantPumpkin()
+			h.moveToNext()
+			
+				
+	
+		
+		
